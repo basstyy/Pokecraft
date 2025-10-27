af20bcf7d5dbacc15e566387f545c2b9babf64b5
basstyy <bastian.armijo@estudiantes.colegiotgs.cl>
2025-10-27 13:38:37 -0300

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -1,7 +1,4 @@
-# Archivo principal de la aplicación.
-# Streamlit detecta automáticamente los archivos en la carpeta 'pages' 
-# y los utiliza para crear la navegación lateral.
-
+# Importación necesaria
 import streamlit as st
 
 # Configuración básica de la página
@@ -13,4 +10,5 @@ st.set_page_config(
 
 pg = st.navigation(["Homepage.py", "Guia.py", "Contacto.py"])
 pg.run()
+
 # st.sidebar.success("Selecciona una página arriba.")
